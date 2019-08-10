5e1f90c9fbbfd9d160b3fedb68e529ac052278fb
Stephen Firth <stephendfirth@gmail.com>
2019-08-10 17:57:23 -0500

    Added basic movement code, updated test level

--- Assets/Scripts/Pawn/RocketCar.cs
+++ Assets/Scripts/Pawn/RocketCar.cs
@@ -4,31 +4,42 @@ using UnityEngine;
 
 public class RocketCar : MonoBehaviour
 {
-    public float turnSpeed, forwardSpeed, maxSpeed;
-    public GameObject pawn;
+    public float turnSpeed, forwardSpeed;
+    public float accelerationPower = 10;
+    public float brakingPower = 5;
+    //Vector3 velocity = new Vector3();
+    float currentSpeed = 0;
+    float maxWheelTurn = 45;
+    float maximumGroundSpeed = 20;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void FixedUpdate()
     {
+        //Get input this frame
         float moveForward = Input.GetAxis("Vertical");
-
         float moveRight = Input.GetAxis("Horizontal");
+        float throttle = Input.GetAxis("Throttle");
+        
+        if(throttle!=0)
+        {
+            currentSpeed = currentSpeed + (throttle*accelerationPower*Time.deltaTime);
+            currentSpeed = Mathf.Clamp(currentSpeed, -maximumGroundSpeed, maximumGroundSpeed);
+        }
+        else
+        {
 
-        Vector3 updatedMove = transform.position;
+            currentSpeed = currentSpeed - (brakingPower * Time.deltaTime);
+            currentSpeed = Mathf.Clamp(currentSpeed, 0, maximumGroundSpeed);
+        }
+     
 
-        float x = updatedMove.x ;
-        float y = updatedMove.y;
-        float z = updatedMove.z + (moveForward * forwardSpeed * Time.deltaTime);
+        Vector3 rotatedVector = Quaternion.AngleAxis((maxWheelTurn*moveRight), Vector3.up) * transform.forward;
 
-        updatedMove = new Vector3(x, y, z);
-
-        transform.position = updatedMove;
-
+        Vector3 targetPosition = (rotatedVector * currentSpeed * Time.deltaTime) + transform.position;
+        transform.LookAt(targetPosition,Vector3.up);
+        transform.position = targetPosition;
+        
+        
     }
 
 
