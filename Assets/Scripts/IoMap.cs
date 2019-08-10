aaae0fe02e119cde56761c9dbdf9314f2c0933bf
TheArkratos <didaskaleinofoba@aim.com>
2019-08-10 16:48:20 -0500

    IInputHandler and IoMap stubs

---
+++ Assets/Scripts/IoMap.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IoMap
+{
+    bool A;
+    bool B;
+    bool X;
+    bool Y;
+    bool Select;
+    bool Start;
+    bool LB;
+    bool RB;
+
+    bool Left;
+    bool Right;
+    bool Up;
+    bool Down;
+
+    bool LeftStick;
+    bool RightStick;
+
+    float Axis1;
+    float Axis2;
+    float Axis3;
+    float Axis4;
+
+    float LT;
+    float RT;
+}
